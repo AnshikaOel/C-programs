37fbbc9a029ee9ea9b2aa617033d64fde3769e22
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2023-04-17 18:22:18 +0530

    Create LeapYear.c

---
+++ LeapYear.c
@@ -0,0 +1,11 @@
+// ENTERED YEAR IS LEAP YEAR OR NOT
+#include <stdio.h>
+
+int main()
+{
+    int year;
+    printf("Enter the year  : ");
+    scanf("%d",&year);
+    ((year%100!=0 && year%4==0)||year%400==0)?(printf("The year is leap year")):(printf("The year is not a leap year"));
+    return 0;
+}
